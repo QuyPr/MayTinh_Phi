8c255c28ea50b9753ae699c309458c7d0fb7e420
tranl <tranl@EVA_01>
2023-12-06 12:26:09 +0700

    Add project files.

---
+++ MayTinh_Phi/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MayTinh_Phi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_HienThiKetQua = new System.Windows.Forms.TextBox();
+            this.btn_Cong = new System.Windows.Forms.Button();
+            this.btn_Tru = new System.Windows.Forms.Button();
+            this.btn_Nhan = new System.Windows.Forms.Button();
+            this.btn_Chia = new System.Windows.Forms.Button();
+            this.btn_AC = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.txt_Sothunhat = new System.Windows.Forms.TextBox();
+            this.txt_Sothuhai = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lblKetqua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_HienThiKetQua
+            // 
+            this.txt_HienThiKetQua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_HienThiKetQua.Enabled = false;
+            this.txt_HienThiKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_HienThiKetQua.Location = new System.Drawing.Point(247, 145);
+            this.txt_HienThiKetQua.Name = "txt_HienThiKetQua";
+            this.txt_HienThiKetQua.Size = new System.Drawing.Size(240, 35);
+            this.txt_HienThiKetQua.TabIndex = 0;
+            // 
+            // btn_Cong
+            // 
+            this.btn_Cong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cong.Location = new System.Drawing.Point(68, 203);
+            this.btn_Cong.Name = "btn_Cong";
+            this.btn_Cong.Size = new System.Drawing.Size(114, 48);
+            this.btn_Cong.TabIndex = 1;
+            this.btn_Cong.Text = "+";
+            this.btn_Cong.UseVisualStyleBackColor = true;
+            this.btn_Cong.Click += new System.EventHandler(this.btn_Cong_Click);
+            // 
+            // btn_Tru
+            // 
+            this.btn_Tru.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Tru.Location = new System.Drawing.Point(227, 203);
+            this.btn_Tru.Name = "btn_Tru";
+            this.btn_Tru.Size = new System.Drawing.Size(114, 48);
+            this.btn_Tru.TabIndex = 1;
+            this.btn_Tru.Text = "-";
+            this.btn_Tru.UseVisualStyleBackColor = true;
+            this.btn_Tru.Click += new System.EventHandler(this.btn_Tru_Click);
+            // 
+            // btn_Nhan
+            // 
+            this.btn_Nhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Nhan.Location = new System.Drawing.Point(373, 203);
+            this.btn_Nhan.Name = "btn_Nhan";
+            this.btn_Nhan.Size = new System.Drawing.Size(114, 48);
+            this.btn_Nhan.TabIndex = 1;
+            this.btn_Nhan.Text = "x";
+            this.btn_Nhan.UseVisualStyleBackColor = true;
+            this.btn_Nhan.Click += new System.EventHandler(this.btn_Nhan_Click);
+            // 
+            // btn_Chia
+            // 
+            this.btn_Chia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Chia.Location = new System.Drawing.Point(557, 203);
+            this.btn_Chia.Name = "btn_Chia";
+            this.btn_Chia.Size = new System.Drawing.Size(114, 48);
+            this.btn_Chia.TabIndex = 1;
+            this.btn_Chia.Text = "/";
+            this.btn_Chia.UseVisualStyleBackColor = true;
+            this.btn_Chia.Click += new System.EventHandler(this.btn_Chia_Click);
+            // 
+            // btn_AC
+            // 
+            this.btn_AC.Location = new System.Drawing.Point(214, 291);
+            this.btn_AC.Name = "btn_AC";
+            this.btn_AC.Size = new System.Drawing.Size(114, 48);
+            this.btn_AC.TabIndex = 1;
+            this.btn_AC.Text = "AC";
+            this.btn_AC.UseVisualStyleBackColor = true;
+            this.btn_AC.Click += new System.EventHandler(this.btn_AC_Click);
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Location = new System.Drawing.Point(391, 291);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(114, 48);
+            this.btn_Thoat.TabIndex = 1;
+            this.btn_Thoat.Text = "Thoat";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // txt_Sothunhat
+            // 
+            this.txt_Sothunhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Sothunhat.Location = new System.Drawing.Point(245, 12);
+            this.txt_Sothunhat.Name = "txt_Sothunhat";
+            this.txt_Sothunhat.Size = new System.Drawing.Size(240, 35);
+            this.txt_Sothunhat.TabIndex = 0;
+            this.txt_Sothunhat.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Sothunhat_KeyPress);
+            // 
+            // txt_Sothuhai
+            // 
+            this.txt_Sothuhai.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Sothuhai.Location = new System.Drawing.Point(245, 66);
+            this.txt_Sothuhai.Name = "txt_Sothuhai";
+            this.txt_Sothuhai.Size = new System.Drawing.Size(240, 35);
+            this.txt_Sothuhai.TabIndex = 0;
+            this.txt_Sothuhai.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Sothuhai_KeyPress);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(131, 22);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(105, 20);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "So thu Nhat";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(135, 76);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(94, 20);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "So thu Hai";
+            // 
+            // lblKetqua
+            // 
+            this.lblKetqua.AutoSize = true;
+            this.lblKetqua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetqua.Location = new System.Drawing.Point(145, 145);
+            this.lblKetqua.Name = "lblKetqua";
+            this.lblKetqua.Size = new System.Drawing.Size(74, 20);
+            this.lblKetqua.TabIndex = 2;
+            this.lblKetqua.Text = "Ket Qua";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKetqua);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btn_AC);
+            this.Controls.Add(this.btn_Chia);
+            this.Controls.Add(this.btn_Nhan);
+            this.Controls.Add(this.btn_Tru);
+            this.Controls.Add(this.btn_Cong);
+            this.Controls.Add(this.txt_Sothunhat);
+            this.Controls.Add(this.txt_Sothuhai);
+            this.Controls.Add(this.txt_HienThiKetQua);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_HienThiKetQua;
+        private System.Windows.Forms.Button btn_Cong;
+        private System.Windows.Forms.Button btn_Tru;
+        private System.Windows.Forms.Button btn_Nhan;
+        private System.Windows.Forms.Button btn_Chia;
+        private System.Windows.Forms.Button btn_AC;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.TextBox txt_Sothunhat;
+        private System.Windows.Forms.TextBox txt_Sothuhai;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lblKetqua;
+    }
+}
+
---
+++ MayTinh_Phi/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayTinh_Phi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_AC_Click(object sender, EventArgs e)
+        {
+           txt_Sothunhat.Clear();
+           txt_Sothuhai.Clear();
+           txt_HienThiKetQua.Clear();
+        }
+
+        private void btn_Cong_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt_Sothunhat.Text, out double sothu1) && (double.TryParse(txt_Sothuhai.Text, out double sothu2)))
+            {
+                double ketqua = 0;
+                ketqua = sothu1 + sothu2;
+                txt_HienThiKetQua.Text =ketqua.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đủ dữ liệu hợp lệ trước khi thực hiện phép cộng.");
+            }
+
+        }
+
+        private void btn_Tru_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt_Sothunhat.Text, out double sothu1) && (double.TryParse(txt_Sothuhai.Text, out double sothu2)))
+            {
+                double ketqua = 0;
+                ketqua = sothu1 - sothu2;
+                txt_HienThiKetQua.Text=ketqua.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đủ dữ liệu hợp lệ trước khi thực hiện phép cộng.");
+            }
+        }
+
+        private void btn_Nhan_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt_Sothunhat.Text, out double sothu1) && (double.TryParse(txt_Sothuhai.Text, out double sothu2)))
+            {
+                double ketqua = 0;
+                ketqua = sothu1 * sothu2;
+                txt_HienThiKetQua.Text=(ketqua.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đủ dữ liệu hợp lệ trước khi thực hiện phép cộng.");
+            }
+        }
+
+        private void btn_Chia_Click(object sender, EventArgs e)
+        {
+            if(double.TryParse(txt_Sothunhat.Text, out double sothu1) && (double.TryParse(txt_Sothuhai.Text, out double sothu2)))
+            {
+                double ketqua = 0;
+                ketqua = sothu1 / sothu2;
+                txt_HienThiKetQua.Text = (ketqua.ToString());
+
+                if(sothu2 == 0)
+                {
+                    MessageBox.Show("kHONG CHIA DUOC CHO 0");
+                    txt_Sothuhai.Clear();
+                    txt_HienThiKetQua.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đủ dữ liệu hợp lệ trước khi thực hiện phép cộng.");
+            }
+        }
+
+        private void txt_Sothunhat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true; // Block the input if it's not a valid character
+            }
+        }
+
+        private void txt_Sothuhai_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true; // Block the input if it's not a valid character
+            }
+        }
+    }
+
+ }
+
